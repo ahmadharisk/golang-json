9955fb18505ab77f665d60d662ec3bcba3e6da95
Peppo <ahmadharisk@gmail.com>
2025-10-18 17:06:15 +0700

    hari 30, belajar golang json implementasi encode

--- encode_json_test.go
+++ encode_json_test.go
@@ -1,1 +1,26 @@
 package golang_json
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func logJson(data interface{}) {
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(bytes))
+}
+
+func TestEncode(t *testing.T) {
+	logJson("peppo")
+	logJson(100)
+	logJson(true)
+	logJson([]string{"peppo", "silpiana"})
+	logJson(map[string]interface{}{
+		"name": "silpiana",
+		"age":  7,
+	})
+}
